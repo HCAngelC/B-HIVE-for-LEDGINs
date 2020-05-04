738d6e5275125aef167d68f00a8d6011747e1098
Heng-Chang Chen <Chen.heng-chang@igh.cnrs.fr>
2020-05-04 13:27:08 +0200

    Update HIV_woRNA.R

--- HIV_woRNA.R
+++ HIV_woRNA.R
@@ -14,18 +14,8 @@ library(forcats)
 hiv_allsamples <- read.table("hiv_allsamples_jurkatchip.txt", header = T) #hiv_allsamples_jurkatchip.txt is available at xxx.
 
 ## SupT1 
-SupT1_0ic_gene_S296_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S296" & cat != "RE" & rna != 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
 SupT1_0ic_gene_S296_woRNA <- hiv_allsamples %>%
 dplyr::filter(rep == "S296" & cat != "RE" & rna == 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
-SupT1_2ic_gene_S297_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S297" & cat != "RE" & rna != 0) %>%
 dplyr::select(gene_name, expscore) %>%
 na.omit()
 
@@ -34,18 +24,8 @@ dplyr::filter(rep == "S297" & cat != "RE" & rna == 0) %>%
 dplyr::select(gene_name, expscore) %>%
 na.omit()
 
-SupT1_5ic_gene_S298_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S298" & cat != "RE" & rna != 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
 SupT1_5ic_gene_S298_woRNA <- hiv_allsamples %>%
 dplyr::filter(rep == "S298" & cat != "RE" & rna == 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
-SupT1_10ic_gene_S299_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S299" & cat != "RE" & rna != 0) %>%
 dplyr::select(gene_name, expscore) %>%
 na.omit()
 
@@ -55,18 +35,8 @@ dplyr::select(gene_name, expscore) %>%
 na.omit()
 
 ## Jurkat  
-Jurkat_0ic_gene_S300_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S300" & cat != "RE" & rna != 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
 Jurkat_0ic_gene_S300_woRNA <- hiv_allsamples %>%
 dplyr::filter(rep == "S300" & cat != "RE" & rna == 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
-Jurkat_2ic_gene_S301_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S301" & cat != "RE" & rna != 0) %>%
 dplyr::select(gene_name, expscore) %>%
 na.omit()
 
@@ -75,18 +45,8 @@ dplyr::filter(rep == "S301" & cat != "RE" & rna == 0) %>%
 dplyr::select(gene_name, expscore) %>%
 na.omit()
 
-Jurkat_5ic_gene_S302_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S302" & cat != "RE" & rna != 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
 Jurkat_5ic_gene_S302_woRNA <- hiv_allsamples %>%
 dplyr::filter(rep == "S302" & cat != "RE" & rna == 0) %>%
-dplyr::select(gene_name, expscore) %>%
-na.omit()
-
-Jurkat_10ic_gene_S303_wRNA <- hiv_allsamples %>%
-dplyr::filter(rep == "S303" & cat != "RE" & rna != 0) %>%
 dplyr::select(gene_name, expscore) %>%
 na.omit()
 
@@ -97,66 +57,34 @@ na.omit()
 
 # aggregate for each unique gene
 ## SupT1
-SupT1_0ic_gen_S296_wRNA_agg <- aggregate(SupT1_0ic_gen_S296_wRNA$expscore, by = list(SupT1_0ic_gen_S296_wRNA$gene_name), FUN = mean)
-names(SupT1_0ic_gen_S296_wRNA_agg) <- c("gene_name", "exp_mean")
-head(SupT1_0ic_gen_S296_wRNA_agg)
-
 SupT1_0ic_gen_S296_woRNA_agg <- aggregate(SupT1_0ic_gen_S296_woRNA$expscore, by = list(SupT1_0ic_gen_S296_woRNA$gene_name), FUN = mean)
 names(SupT1_0ic_gen_S296_woRNA_agg) <- c("gene_name", "exp_mean")
 head(SupT1_0ic_gen_S296_woRNA_agg)
-
-SupT1_2ic_gen_S297_wRNA_agg <- aggregate(SupT1_2ic_gen_S297_wRNA$expscore, by = list(SupT1_2ic_gen_S297_wRNA$gene_name), FUN = mean)
-names(SupT1_2ic_gen_S297_wRNA_agg) <- c("gene_name", "exp_mean")
-head(SupT1_2ic_gen_S297_wRNA_agg)
 
 SupT1_2ic_gen_S297_woRNA_agg <- aggregate(SupT1_2ic_gen_S297_woRNA$expscore, by = list(SupT1_2ic_gen_S297_woRNA$gene_name), FUN = mean)
 names(SupT1_2ic_gen_S297_woRNA_agg) <- c("gene_name", "exp_mean")
 head(SupT1_2ic_gen_S297_woRNA_agg)
 
-SupT1_5ic_gen_S298_wRNA_agg <- aggregate(SupT1_5ic_gen_S298_wRNA$expscore, by = list(SupT1_5ic_gen_S298_wRNA$gene_name), FUN = mean)
-names(SupT1_5ic_gen_S298_wRNA_agg) <- c("gene_name", "exp_mean")
-head(SupT1_5ic_gen_S298_wRNA_agg)
-
 SupT1_5ic_gen_S298_woRNA_agg <- aggregate(SupT1_5ic_gen_S298_woRNA$expscore, by = list(SupT1_5ic_gen_S298_woRNA$gene_name), FUN = mean)
 names(SupT1_5ic_gen_S298_woRNA_agg) <- c("gene_name", "exp_mean")
 head(SupT1_5ic_gen_S298_woRNA_agg)
-
-SupT1_10ic_gen_S299_wRNA_agg <- aggregate(SupT1_10ic_gen_S299_wRNA$expscore, by = list(SupT1_10ic_gen_S299_wRNA$gene_name), FUN = mean)
-names(SupT1_10ic_gen_S299_wRNA_agg) <- c("gene_name", "exp_mean")
-head(SupT1_10ic_gen_S299_wRNA_agg)
 
 SupT1_10ic_gen_S299_woRNA_agg <- aggregate(SupT1_10ic_gen_S299_woRNA$expscore, by = list(SupT1_10ic_gen_S299_woRNA$gene_name), FUN = mean)
 names(SupT1_10ic_gen_S299_woRNA_agg) <- c("gene_name", "exp_mean")
 head(SupT1_10ic_gen_S299_woRNA_agg)
 
 ## Jurkat
-Jurkat_0ic_gen_S300_wRNA_agg <- aggregate(Jurkat_0ic_gen_S300_wRNA$expscore, by = list(Jurkat_0ic_gen_S300_wRNA$gene_name), FUN = mean)
-names(Jurkat_0ic_gen_S300_wRNA_agg) <- c("gene_name", "exp_mean")
-head(Jurkat_0ic_gen_S300_wRNA_agg)
-
 Jurkat_0ic_gen_S300_woRNA_agg <- aggregate(Jurkat_0ic_gen_S300_woRNA$expscore, by = list(Jurkat_0ic_gen_S300_woRNA$gene_name), FUN = mean)
 names(Jurkat_0ic_gen_S300_woRNA_agg) <- c("gene_name", "exp_mean")
 head(Jurkat_0ic_gen_S300_woRNA_agg)
-
-Jurkat_2ic_gen_S301_wRNA_agg <- aggregate(Jurkat_2ic_gen_S301_wRNA$expscore, by = list(Jurkat_2ic_gen_S301_wRNA$gene_name), FUN = mean)
-names(Jurkat_2ic_gen_S301_wRNA_agg) <- c("gene_name", "exp_mean")
-head(Jurkat_2ic_gen_S301_wRNA_agg)
 
 Jurkat_2ic_gen_S301_woRNA_agg <- aggregate(Jurkat_2ic_gen_S301_woRNA$expscore, by = list(Jurkat_2ic_gen_S301_woRNA$gene_name), FUN = mean)
 names(Jurkat_2ic_gen_S301_woRNA_agg) <- c("gene_name", "exp_mean")
 head(Jurkat_2ic_gen_S301_woRNA_agg)
 
-Jurkat_5ic_gen_S302_wRNA_agg <- aggregate(Jurkat_5ic_gen_S302_wRNA$expscore, by = list(Jurkat_5ic_gen_S302_wRNA$gene_name), FUN = mean)
-names(Jurkat_5ic_gen_S302_wRNA_agg) <- c("gene_name", "exp_mean")
-head(Jurkat_5ic_gen_S302_wRNA_agg)
-
 Jurkat_5ic_gen_S302_woRNA_agg <- aggregate(Jurkat_5ic_gen_S302_woRNA$expscore, by = list(Jurkat_5ic_gen_S302_woRNA$gene_name), FUN = mean)
 names(Jurkat_5ic_gen_S302_woRNA_agg) <- c("gene_name", "exp_mean")
 head(Jurkat_5ic_gen_S302_woRNA_agg)
-
-Jurkat_10ic_gen_S303_wRNA_agg <- aggregate(Jurkat_10ic_gen_S303_wRNA$expscore, by = list(Jurkat_10ic_gen_S303_wRNA$gene_name), FUN = mean)
-names(Jurkat_10ic_gen_S303_wRNA_agg) <- c("gene_name", "exp_mean")
-head(Jurkat_10ic_gen_S303_wRNA_agg)
 
 Jurkat_10ic_gen_S303_woRNA_agg <- aggregate(Jurkat_10ic_gen_S303_woRNA$expscore, by = list(Jurkat_10ic_gen_S303_woRNA$gene_name), FUN = mean)
 names(Jurkat_10ic_gen_S303_woRNA_agg) <- c("gene_name", "exp_mean")
