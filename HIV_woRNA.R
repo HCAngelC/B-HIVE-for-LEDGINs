112b274f40eef57801f46c1d211b0257498a341f
Heng-Chang Chen <Chen.heng-chang@igh.cnrs.fr>
2020-05-05 17:37:42 +0200

    Update HIV_woRNA.R

--- HIV_woRNA.R
+++ HIV_woRNA.R
@@ -129,3 +129,82 @@ kkenrich.Jurkat_0ic_gen_S300_woRNA_agg.entrezID <- enrichKEGG(gene = Jurkat_0ic_
 kkenrich.Jurkat_2ic_gen_S301_woRNA_agg.entrezID <- enrichKEGG(gene = Jurkat_2ic_gen_S301_woRNA_agg.entrezID, organism = 'hsa', pvalueCutoff = 1)
 kkenrich.Jurkat_5ic_gen_S302_woRNA_agg.entrezID <- enrichKEGG(gene = Jurkat_5ic_gen_S302_woRNA_agg.entrezID, organism = 'hsa', pvalueCutoff = 1)
 kkenrich.Jurkat_10ic_gen_S303_woRNA_agg.entrezID <- enrichKEGG(gene = Jurkat_10ic_gen_S303_woRNA_agg.entrezID, organism = 'hsa', pvalueCutoff = 1)
+
+########################################################
+# import rna-seq fc file
+rnaseq_supt1_fc <- read.table("rnaseq_supt1_fc.txt", header = T)
+rnaseq_jurkat_fc <- read.table("rnaseq_jurkat_fc.txt", header = T)
+
+## associate rna-seq file with woRNA.entrezID file
+SupT1_0ic_gen_S296_woRNA_agg.entrezID.rnaseq <- merge(SupT1_0ic_gen_S296_woRNA_agg.entrezID, rnaseq_supt1_fc, b = "SYMBOL")
+SupT1_2ic_gen_S297_woRNA_agg.entrezID.rnaseq <- merge(SupT1_2ic_gen_S297_woRNA_agg.entrezID, rnaseq_supt1_fc, b = "SYMBOL")
+SupT1_5ic_gen_S298_woRNA_agg.entrezID.rnaseq <- merge(SupT1_5ic_gen_S298_woRNA_agg.entrezID, rnaseq_supt1_fc, b = "SYMBOL")
+SupT1_10ic_gen_S299_woRNA_agg.entrezID.rnaseq <- merge(SupT1_10ic_gen_S299_woRNA_agg.entrezID, rnaseq_supt1_fc, b = "SYMBOL")
+
+Jurkat_0ic_gen_S300_woRNA_agg.entrezID.rnaseq <- merge(Jurkat_0ic_gen_S300_woRNA_agg.entrezID, rnaseq_jurkat_fc, b = "SYMBOL")
+Jurkat_2ic_gen_S301_woRNA_agg.entrezID.rnaseq <- merge(Jurkat_2ic_gen_S301_woRNA_agg.entrezID, rnaseq_jurkat_fc, b = "SYMBOL")
+Jurkat_5ic_gen_S302_woRNA_agg.entrezID.rnaseq <- merge(Jurkat_5ic_gen_S302_woRNA_agg.entrezID, rnaseq_jurkat_fc, b = "SYMBOL")
+Jurkat_10ic_gen_S303_woRNA_agg.entrezID.rnaseq <- merge(Jurkat_10ic_gen_S303_woRNA_agg.entrezID, rnaseq_jurkat_fc, b = "SYMBOL")
+
+### prepare geneList based on endogenous gene expression
+#### SupT1
+geneList_supt1_0ic_gen_S296_woRNA <- log10(SupT1_0ic_gen_S296_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_supt1_0ic_gen_S296_woRNA) <- as.character(SupT1_0ic_gen_S296_woRNA_agg.entrezID.rnaseq[,3])
+geneList_supt1_0ic_gen_S296_woRNA <- sort(geneList_supt1_0ic_gen_S296_woRNA, decreasing = T)
+
+geneList_supt1_2ic_gen_S297_woRNA <- log10(SupT1_2ic_gen_S297_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_supt1_2ic_gen_S297_woRNA) <- as.character(SupT1_2ic_gen_S297_woRNA_agg.entrezID.rnaseq[,3])
+geneList_supt1_2ic_gen_S297_woRNA <- sort(geneList_supt1_2ic_gen_S297_woRNA, decreasing = T)
+
+geneList_supt1_5ic_gen_S298_woRNA <- log10(SupT1_5ic_gen_S298_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_supt1_5ic_gen_S298_woRNA) <- as.character(SupT1_5ic_gen_S298_woRNA_agg.entrezID.rnaseq[,3])
+geneList_supt1_5ic_gen_S298_woRNA <- sort(geneList_supt1_5ic_gen_S298_woRNA, decreasing = T)
+
+geneList_supt1_10ic_gen_S299_woRNA <- log10(SupT1_10ic_gen_S299_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_supt1_10ic_gen_S299_woRNA) <- as.character(SupT1_10ic_gen_S299_woRNA_agg.entrezID.rnaseq[,3])
+geneList_supt1_10ic_gen_S299_woRNA <- sort(geneList_supt1_10ic_gen_S299_woRNA, decreasing = T)
+
+#### Jurkat
+geneList_jurkat_0ic_gen_S300_woRNA <- log10(Jurkat_0ic_gen_S300_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_jurkat_0ic_gen_S300_woRNA) <- as.character(Jurkat_0ic_gen_S300_woRNA_agg.entrezID.rnaseq[,3])
+geneList_jurkat_0ic_gen_S300_woRNA <- sort(geneList_jurkat_0ic_gen_S300_woRNA, decreasing = T)
+
+geneList_jurkat_2ic_gen_S301_woRNA <- log10(Jurkat_2ic_gen_S301_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_jurkat_2ic_gen_S301_woRNA) <- as.character(Jurkat_2ic_gen_S301_woRNA_agg.entrezID.rnaseq[,3])
+geneList_jurkat_2ic_gen_S301_woRNA <- sort(geneList_jurkat_2ic_gen_S301_woRNA, decreasing = T)
+
+geneList_jurkat_5ic_gen_S302_woRNA <- log10(Jurkat_5ic_gen_S302_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_jurkat_5ic_gen_S302_woRNA) <- as.character(Jurkat_5ic_gen_S302_woRNA_agg.entrezID.rnaseq[,3])
+geneList_jurkat_5ic_gen_S302_woRNA <- sort(geneList_jurkat_5ic_gen_S302_woRNA, decreasing = T)
+
+geneList_jurkat_10ic_gen_S303_woRNA <- log10(Jurkat_10ic_gen_S303_woRNA_agg.entrezID.rnaseq[,6] + 1)
+names(geneList_jurkat_10ic_gen_S303_woRNA) <- as.character(Jurkat_10ic_gen_S303_woRNA_agg.entrezID.rnaseq[,3])
+geneList_jurkat_10ic_gen_S303_woRNA <- sort(geneList_jurkat_10ic_gen_S303_woRNA, decreasing = T)
+
+########################################################
+# cnetplot
+## SupT1
+SupT1_0ic_gen_S296_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.SupT1_0ic_gen_S296_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(SupT1_0ic_gen_S296_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_supt1_0ic_gen_S296_woRNA)
+
+SupT1_2ic_gen_S297_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.SupT1_2ic_gen_S297_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(SupT1_2ic_gen_S297_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_supt1_2ic_gen_S297_woRNA)
+
+SupT1_5ic_gen_S298_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.SupT1_5ic_gen_S298_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(SupT1_5ic_gen_S298_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_supt1_5ic_gen_S298_woRNA)
+
+SupT1_10ic_gen_S299_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.SupT1_10ic_gen_S299_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(SupT1_10ic_gen_S299_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_supt1_10ic_gen_S299_woRNA)
+
+## Jurkat
+Jurkat_0ic_gen_S300_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.Jurkat_0ic_gen_S300_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(Jurkat_0ic_gen_S300_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_jurkat_0ic_gen_S300_woRNA)
+
+Jurkat_2ic_gen_S301_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.Jurkat_2ic_gen_S301_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(Jurkat_2ic_gen_S301_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_jurkat_2ic_gen_S301_woRNA)
+
+Jurkat_5ic_gen_S302_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.Jurkat_5ic_gen_S302_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(Jurkat_5ic_gen_S302_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_jurkat_5ic_gen_S302_woRNA)
+
+Jurkat_10ic_gen_S303_woRNA_agg.entrezID.kk.reSet <- setReadable(kkenrich.Jurkat_10ic_gen_S303_woRNA_agg.entrezID, "org.Hs.eg.db", "ENTREZID")
+cnetplot(Jurkat_10ic_gen_S303_woRNA_agg.entrezID.kk.reSet, foldChange = geneList_jurkat_10ic_gen_S303_woRNA)
